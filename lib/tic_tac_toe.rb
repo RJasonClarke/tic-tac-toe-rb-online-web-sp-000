d10f056e18c4c69239d722dd90488b991fa12b37
Robert Clarke <r.jason.clarke@gmail.com>
2021-03-24 15:51:30 +0000

    Automatically backed up by Learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,65 @@
+WIN_COMBINATIONS = [
+[0,1,2],
+[3,4,5],
+[6,7,8],
+[0,3,6],
+[1,4,7],
+[2,5,8],
+[0,4,8],
+[2,4,6]
+]
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(input)
+  index = input.to_i
+  index -= 1
+  return index
+end
+
+def move(board, index, player_token)
+  character = board[index]
+  return board
+end
+
+def position_taken?(board, index)
+  if board[index] == "" || board[index] == " " || board[index] == nil
+      return false
+  else
+      return true
+  end
+end
+
+def valid_move?(board, index)
+end
+
+def turn_count
+  counter = 0
+end
+
+def current_player(board)
+end
+
+def turn
+end
+
+def won?
+end
+
+def full?
+end
+
+def draw?
+end
+
+def over?
+end
+
+def winner
+end
